6735eff7e5b8353c2805a1768b661c85404546b0
m.bebars <66588352+mahmoud-bebars@users.noreply.github.com>
2024-02-29 04:37:18 +0200

    Router Inital
    Adding all the Routes in One Source as array to be maooed in Server when Starting

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+module.exports.routes = [
+  { routeName: "Activation", prefix: "/api/active" },
+  { routeName: "Authenticate", prefix: "/api/auth" },
+  { routeName: "Authorize", prefix: "/api/verfiy" },
+  { routeName: "Meta", prefix: "/api/meta" },
+  { routeName: "OTP", prefix: "/api/otp" },
+  { routeName: "Password", prefix: "/api/password" },
+  { routeName: "Profile", prefix: "/api/profile" },
+];
